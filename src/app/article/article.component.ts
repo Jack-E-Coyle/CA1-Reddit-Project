3a08a121c3a7a038d1a227e9060bf7e6a2bcef80
Jack Coyle <60261884+Jack-E-Coyle@users.noreply.github.com>
2021-02-19 21:08:52 +0000

    Part 5

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -7,11 +7,11 @@ import { Article } from './article.model';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  @Input() article: Article;
 
-  article: Article;
 
   constructor() { 
-    this.article = new Article("Angular", "http://angular.io", 10)
+    //this.article = new Article("Angular", "http://angular.io", 10)
   }
 
   ngOnInit(): void {
